5de03d357b2d9a62f4af7ec3bb607bd79e8b54a7
Mike Tabler <mt-224254@hs-weingarten.de>
2023-10-13 15:46:00 +0200

    confirmFunktion hinzugefügt

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,33 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import { RouterLink, RouterView } from 'vue-router';
+
+export default{
+  data(){
+    return{
+      text: ""
+    }
+  },
+  methods:{
+    confirmFunktion(){
+      this.text = "Allow access to your camera";
+      if(confirm(this.text)==true){
+        this.text = "Allowed!";
+      }else{
+        this.text ="Declined!";
+      }
+    }
+  }
+}
 </script>
 
 <template>
   <header>
     <h1>Barcode Scanner</h1>
   </header>
+  <button @click="confirmFunktion">ConfirmButton</button>
+  <p>{{ text }}</p>
 </template>
 
-<style scoped>
+<style>
 
 </style>
